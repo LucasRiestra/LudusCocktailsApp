8d55b33859f1c6c1b1ba451d16f616e120a3e6f9
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-02-20 09:57:56 +0100

    refact/filters

--- src/Utils/FilterCocktails.ts
+++ src/Utils/FilterCocktails.ts
@@ -1,49 +1,38 @@
 export const filterCocktails = (allCocktails: any[], searchTerm: string) => {
-
   const searchWords = searchTerm.toLowerCase().split(' ');
 
-    let matchingCocktails = allCocktails.filter((cocktail: any) => {
-      let ingredientCount = 0;
-      for (let i = 1; i <= 15; i++) {
-        if (cocktail[`strIngredient${i}`]) {
-          ingredientCount++;
-        }
+  const isMatch = (text: string) => 
+    searchWords.every(word => 
+      text.toLowerCase().split(' ').some((textWord: string) => textWord.startsWith(word))
+    );
+
+  let matchingCocktails = allCocktails.filter((cocktail: any) => {
+    let ingredientCount = 0;
+    for (let i = 1; i <= 15; i++) {
+      if (cocktail[`strIngredient${i}`]) {
+        ingredientCount++;
       }
-      if (ingredientCount > 6) {
-        return false;
-      }
-      
-      if (searchTerm.toLowerCase() === 'no alcohol' || searchTerm.toLowerCase() === 'without alcohol') {
-        return cocktail.strAlcoholic === 'Non alcoholic';
-      }
-      
+    }
+    if (ingredientCount > 6) {
+      return false;
+    }
+    
+    if (searchTerm.toLowerCase() === 'no alcohol' || searchTerm.toLowerCase() === 'without alcohol') {
+      return cocktail.strAlcoholic === 'Non alcoholic';
+    }
+    
     for (let i = 1; i <= 15; i++) {
       const ingredient = cocktail[`strIngredient${i}`];
-      if (ingredient && searchWords.every(word => ingredient.toLowerCase().split(' ').some((ingredientWord: string) => ingredientWord.startsWith(word)))) {
+      if (ingredient && isMatch(ingredient)) {
         return true;
       }
     }
 
-    if (searchWords.every(word => cocktail.strDrink.toLowerCase().split(' ').some((drinkWord: string) => drinkWord.startsWith(word)))) {
+    if (isMatch(cocktail.strDrink)) {
       return true;
     }
     return false;
-  });
+  })
   
-    if (searchTerm.toLowerCase() === 'no alcohol' || searchTerm.toLowerCase() === 'without alcohol') {
-      matchingCocktails = matchingCocktails.sort(() => 0.5 - Math.random()).slice(0, 6);
-    } else {
-  
-      matchingCocktails.sort((a, b) => {
-        if (a.strAlcoholic === 'Non alcoholic' && b.strAlcoholic !== 'Non alcoholic') {
-          return -1;
-        }
-        if (a.strAlcoholic !== 'Non alcoholic' && b.strAlcoholic === 'Non alcoholic') {
-          return 1;
-        }
-        return 0;
-      });
-    }
-  
-    return matchingCocktails;
-  };
+  return matchingCocktails;
+};
